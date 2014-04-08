c0fd36bfe62bd34200135cc6b73987a80b6e4e02
Riley Shaw <rileyjshaw@gmail.com>
2014-04-08 18:46:20 -0400

    Prevent htmlmin from fucking up my favicons

--- Gruntfile.js
+++ Gruntfile.js
@@ -184,7 +184,7 @@ module.exports = function (grunt) {
         options: {
           collapseWhitespace: true,
           collapseBooleanAttributes: true,
-          removeAttributeQuotes: true,
+          removeAttributeQuotes: false,
           removeRedundantAttributes: true
         },
         files: [{
